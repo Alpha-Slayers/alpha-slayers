9d61ad4d5cd46e3f089760f7066972572c1abded
Edmond Doan <edmonddoan@gmail.com>
2022-11-08 11:38:31 -0600

    navbar works but needs CSS

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-
+    <Navbar/>
     
       <main className={styles.main}>
         <h1 className={styles.title}>Alpha Slayers Guild</h1>
