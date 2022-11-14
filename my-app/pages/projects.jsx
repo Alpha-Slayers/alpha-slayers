ca73f7fa9cd5e8fe92ae2bb020cf81ff107476cd
Edmond Doan <edmonddoan@gmail.com>
2022-11-13 23:51:58 -0600

    added landing pages on other pages

--- my-app/pages/projects.jsx
+++ my-app/pages/projects.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import styles from '../styles/Home.module.css'
 import NavContent from './Navbar/NavContent';
 
-export default function Navbar() {
-    return(
+export default function Projects() {
+    return(<div className={styles.container}>
+
         <NavContent/>
+        <main className={styles.main}>
+        
+          <h1 className={styles.title}>
+            Projects
+          </h1>
+  
+          <p className={styles.description}>
+            Coming Soon.
+          </p>
+          
+       
+        </main>
+  
+
+      </div>
+
+        
     )
 }
